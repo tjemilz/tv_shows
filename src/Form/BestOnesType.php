12c49acd6fdcf30af05dd1554d3d7a337a350232
Emilien Fourgnier <efourgnier94370@gmail.com>
2024-10-30 19:11:39 +0100

    Fin de l'étape 16

--- src/Form/BestOnesType.php
+++ src/Form/BestOnesType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\TvShow;
 use App\Entity\BestOnes;
+use App\Repository\TvShowRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -11,13 +14,34 @@ class BestOnesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        // Récupère le membre via l'option 'data'
+        $bestones = $options['data'] ?? null;
+        $member = $bestones->getCreator(); 
+
         $builder
             ->add('description')
             ->add('published')
             ->add('creator', null, [
                 'disabled' => true,])
-            ->add('tvshows')
-        ;
+            ->add('tvshows', EntityType::class,
+            [
+                // avec 'by_reference' => false, sauvegarde les modifications
+                'by_reference' => false,
+                // classe pas obligatoire
+                'class' => TvShow::class,
+                // permet sélection multiple
+                'multiple' => true,
+                // affiche sous forme de checkboxes
+                'expanded' => true,
+                'query_builder' => function (TvShowRepository $er) use ($member) {
+                                      return $er->createQueryBuilder('o')
+                                                ->leftJoin('o.onlineCatalog', 'i')
+                                                ->leftJoin('i.member', 'm')
+                                                ->andWhere('m.id = :memberId')
+                                                ->setParameter('memberId', $member->getId())
+                                                ;}
+                
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
